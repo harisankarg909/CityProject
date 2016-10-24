79de36b9c4f47f8e75e13eb4bae4338f7b76829c
Harisankar G <harisankarg909@gmail.com>
2016-10-24 22:30:02 +0530

    My Family Project

---
+++ index.js
@@ -0,0 +1,18 @@
+var exp = require('express');
+var app = exp();
+var routes = require('./routes/routes.js');
+
+app.set('view engine', 'ejs');
+app.use(exp.static(__dirname+'/public'));
+
+
+app.get('/sweden', routes.sweden);
+app.get('/amsterdam', routes.amsterdam);
+app.get('/paris', routes.paris);
+
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+
+    console.log("Http server is now listening on port "+port); 
+})
---
+++ routes/routes.js
@@ -0,0 +1,30 @@
+var fs = require('fs');
+var input = __dirname+'/../public';
+console.log(input);
+
+
+exports.sweden = function(req, res){
+
+    var filearray = new Array();
+    filearray = fs.readdirSync(input+'/Sweden', ['**.JPG']);
+
+    res.render('sweden', {title:"::::: SWEDEN :::::", headline:"Sweden is a Scandinavian nation with thousands of coastal islands and inland lakes, along with vast boreal forests and glaciated mountains. Its principal cities, eastern capital Stockholm and southwestern Gothenburg and Malmö, are all coastal. Stockholm is built on 14 islands. It has more than 50 bridges, as well as the medieval old town, Gamla Stan, royal palaces and museums such as open-air Skansen.", country:"sweden", filearr:filearray})
+    console.log(filearray.length+"array length from routes.js");
+}
+
+exports.amsterdam= function(req, res){
+    var filearray = new Array();
+    filearray = fs.readdirSync(input+'/Amsterdam', ['**.JPG']);
+    res.render('amsterdam', {title:"::::: Amsterdam :::::", headline:"Amsterdam is the Netherlands’ capital, known for its artistic heritage, elaborate canal system and narrow houses with gabled facades, legacies of the city’s 17th-century Golden Age. Its Museum District houses the Van Gogh Museum, works by Rembrandt and Vermeer at the Rijksmuseum, and modern art at the Stedelijk. Cycling is key to the city’s character, and there are numerous bike paths.", country:"amsterdam", filearr:filearray});
+
+
+};
+
+exports.paris= function(req, res){
+
+     var filearray = new Array();
+    filearray = fs.readdirSync(input+'/Paris', ['**.JPG']);
+    res.render('paris', {title:"::::: Paris :::::", headline:"Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture and designer boutiques along the Rue du Faubourg Saint-Honoré.", country:"paris", filearr:filearray});
+
+
+}
